283b2c7216922593bb9fc0931f559b618a1e1a48
seokangyoun <tjrkddbs104@naver.com>
2017-07-07 23:36:48 -0400

    add sum c func

--- sum_test.c
+++ sum_test.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include "sum.h"
 
+int sum(int n){
+	int res = 0;
+	for (int i =1; i <= n; ++i){
+		res += i;
+	}
+	return res;
+}
+
 int main(){
 	int n;
 	sum(n);
+
 	return 0;
 }
